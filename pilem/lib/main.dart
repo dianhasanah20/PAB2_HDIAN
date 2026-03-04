931303752a754fba060d811e9cc70d9f60566807
dianhasanah20 <dianhasanah_2428250036@mhs.mdp.ac.id>
2026-03-04 12:32:22 +0700

    pilem

---
+++ pilem/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:pilem/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:pilem/screens/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pilem',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
+    );
+  }
+}
